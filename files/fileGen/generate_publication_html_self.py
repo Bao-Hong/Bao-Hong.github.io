d9e093ac71b01250e5e2a2c1ee07be4b73cc2a8c
Hong Bao <Paul.hhhhb@gmail.com>
2024-10-24 00:30:33 +0800

    2024/10/24

--- files/fileGen/generate_publication_html_self.py
+++ files/fileGen/generate_publication_html_self.py
@@ -1,13 +1,5 @@
 import re
 
-# Input: Your plain text list of publications
-input_text = """
-Hong, B.+, Chen J.+, Huang, W.J., & Li L.* (In review). Serial dependence in smooth pursuit eye movements of preadolescent children and adults
-Hong, B., Chen J., & Li L.* (To be submitted). Temporal dynamics of serial dependence in ocular tracking
-Huang, W.J.+, Chen J.+, Hong, B., Wang, Y., S., Zuo, X., N.*, & Li, L.* (To be submitted). Investigating Brain Structural Correlates of Ocular Tracking in Preadolescent Children and Young Adults.
-Hong, B., Zhang, L., & Sun, H. (2019). Measurement of the Vertical Spatial Metaphor of Power Concepts Using the Implicit Relational Assessment Procedure. Frontiers in Psychology, 10, 1422. https://doi.org/10.3389/fpsyg.2019.01422
-"""
-
 # Regular expression to parse the input text
 pattern = re.compile(
     r"(?P<authors>.+?)\s\((?P<status>In review|To be submitted|\d{4})\)\.\s"
@@ -17,6 +9,17 @@ pattern = re.compile(
 # Separate entries into two categories
 preprints = []
 published = []
+
+# Prompt the user to input their publications
+print("Enter your list of publications (press Enter twice to finish):")
+
+# Collect user input line by line
+input_text = ""
+while True:
+    line = input()
+    if not line:  # Stop when the user presses Enter on an empty line
+        break
+    input_text += line + "\n"
 
 # Process the input text line by line
 for line in input_text.strip().splitlines():
@@ -46,35 +49,30 @@ def generate_html_list(entries):
     html_list += "  </ul>\n"
     return html_list
 
-# Generate the final HTML content
-html_content = f"""
----
-layout: archive
-title: "Publications"
-permalink: /publications/
-author_profile: true
----
-
-{{% if site.author.googlescholar %}}
-  <div class="wordwrap">
-    You can also find my articles on 
-    <a href="{{{{ site.author.googlescholar }}}}">Google Scholar</a>.
-  </div>
-{{% endif %}}
-
-{{% include base_path %}}
-
-<h2>Preprints & Manuscripts Submitted or in Preparation:</h2>
-{generate_html_list(preprints)}
-
-<h2>Published Works:</h2>
-{generate_html_list(published)}
-"""
+# Generate the final HTML content without unnecessary newlines
+html_content = (
+    "---\n"
+    "layout: archive\n"
+    "title: \"Publications\"\n"
+    "permalink: /publications/\n"
+    "author_profile: true\n"
+    "---\n\n"
+    "{% if site.author.googlescholar %}\n"
+    "  <div class=\"wordwrap\">\n"
+    "    You can also find my articles on"
+    " <a href=\"{{ site.author.googlescholar }}\">my Google Scholar</a>.\n"
+    "  </div>\n"
+    "{% endif %}\n\n"
+    "{% include base_path %}\n\n"
+    "<h2>Preprints & Manuscripts Submitted or in Preparation:</h2>\n"
+    f"{generate_html_list(preprints)}\n"
+    "<h2>Published Works:</h2>\n"
+    f"{generate_html_list(published)}"
+)
 
 # Save the generated HTML to publications.html
 output_path = "../../_pages/publications.html"
 with open(output_path, "w", encoding="utf-8") as f:
-    f.write(html_content)
+    f.write(html_content.strip())  # Use strip() to remove any leading/trailing whitespace
 
 print(f"\nGenerated {output_path} successfully!")
-
